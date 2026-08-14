10242d5e6387688c18f15db9d2f27dc3efcfb64e
muralivmuraliv89@gmail.com <muralivmuraliv89@gmail.com>
2026-08-14 21:25:29 +0530

    all about js variables

---
+++ JavaScript/variables/style.js
@@ -0,0 +1,30 @@
+console.log("hello world")
+
+var a=10
+var b=12
+
+var c="Hello World"
+let d=10.10
+console.log(typeof d)
+const e=101
+
+let f=100
+
+{
+    let f=200
+    console.log(f)
+}
+
+let o = {
+    "name":"John",
+    "salary":10000
+}
+console.log(o.name)
+console.log(o)
+o.salary=12000
+console.log(o)
+console.log(f)
+console.log(typeof a+ typeof b)
+console.log(a+b)
+console.log(c)
+console.log(e)
