97a1eea55db0ebde6d7c5ad425f7b7b4a7ea5919
muralivmuraliv89@gmail.com <muralivmuraliv89@gmail.com>
2026-08-15 12:50:04 +0530

    conditional statements

---
+++ JavaScript/conditional/style.js
@@ -0,0 +1,37 @@
+// console.log("hello world")
+
+// let age=18
+
+// if(age>=18){
+//     console.log("You Can drive A car")
+// }
+// else if(age>10 && age<18){
+//     console.log("You can drive only Bikes")   
+// }
+// else{
+//     console.log("Hello bro your still a child")
+// } 
+
+var a=10
+var b=10
+
+// console.log(a>b)
+// console.log(a<b)
+// console.log(a==b)
+// console.log(a===b)
+// console.log(a!=b)
+// console.log(a!==b)
+// console.log(a<=b)
+// console.log(a>=b)
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+
+// console.log( a > b ? a : b)
+
+var a=20
+var c= a > b ? (a-b) : (b-a)
+ 
+console.log(c)
