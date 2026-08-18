385babacea6e28b92daa9b66035d0736e3711fce
muralivmuraliv89@gmail.com <muralivmuraliv89@gmail.com>
2026-08-18 21:29:20 +0530

    Strings in java

---
+++ JavaScript/strings/string.js
@@ -0,0 +1,16 @@
+console.log("All About Strings")
+
+let n="Murali"
+let a="Hello"
+console.log(n.length)
+console.log(n.toUpperCase())
+console.log(n.toLowerCase())
+console.log(n.charAt(2))
+console.log(n.endsWith("li"))
+console.log(n.startsWith("ur"))
+console.log(n.replace("ra","RA"))
+console.log(n.concat(" ",a))
+console.log(n[1])
+console.log(n.slice(1,3))
+console.log(n.at(2))
+console.log(n.indexOf("l"))
