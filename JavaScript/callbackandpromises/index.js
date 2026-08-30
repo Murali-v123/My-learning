b972e61173e387e6f8dfda0859542ed7b4782489
muralivmuraliv89@gmail.com <muralivmuraliv89@gmail.com>
2026-08-30 21:28:38 +0530

    Promises and callback

--- JavaScript/callbackandpromises/index.js
+++ JavaScript/callbackandpromises/index.js
@@ -1,1 +1,27 @@
-alert("hello")
+// 1. Asynchronous execution demonstration
+// alert("hello")
+console.log("sentence 1");
+console.log("sentence 2");
+
+setTimeout(() => {
+    console.log("Under timeout function");
+}, 2000);
+
+
+const fn = () => { 
+    console.log("hello world");
+}
+const callback = (arg,fn) =>{
+    console.log(arg);
+    fn()
+}
+
+const loadscript = (src,callback) =>{
+    let sc=document.createElement("script")
+    sc.src=src
+    // sc.onload=callback("hello") or
+    sc.onload=()=>callback("sucessful",fn) 
+    document.head.append(sc)
+}
+
+loadscript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js",callback)
