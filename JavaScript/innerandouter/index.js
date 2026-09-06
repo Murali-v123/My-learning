b53d708c5aa0273825165da35aa0659b9f1e74f4
muralivmuraliv89@gmail.com <muralivmuraliv89@gmail.com>
2026-09-06 21:27:02 +0530

    inner and outer functions

---
+++ JavaScript/innerandouter/index.js
@@ -0,0 +1,8 @@
+console.log(hey.innerHTML); 
+console.log(hey.outerHTML); 
+console.log(document.body.textContent);
+console.log(document.body.firstChild);
+console.log(document.body.firstElementChild);
+let x=document.body.getElementsByTagName("span")[0]
+console.log(x);
+console.dir(x);
